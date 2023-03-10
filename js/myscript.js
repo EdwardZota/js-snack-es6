e3189249594feba72ae7f1d21afd950d79c89fe8
Edward Zota <zotaedward@gmail.com>
2023-03-10 14:30:22 +0100

    Snack 2 completato

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,6 @@
+/********************************
+ Snack 1
+ *******************************/
 const biciclette = [
     {
         nome:'MountainBike',
@@ -27,4 +30,52 @@ biciclette.forEach(bicicletta =>{
     if(bicicletta.peso == valoreMinore){
         console.log(`La bicicletta con il peso minore è la ${bicicletta.nome} con un peso di ${bicicletta.peso}`);
     }
-})
+});
+
+/********************************
+ Snack 2
+ *******************************/
+
+ const squadre = [
+    {
+        nome:'Milan',
+        punti_fatti:0,
+        falli_subiti:0,
+    },
+    {
+        nome:'Juve',
+        punti_fatti:0,
+        falli_subiti:0,
+    },
+    {
+        nome:'Inter',
+        punti_fatti:0,
+        falli_subiti:0,
+    },
+    {
+        nome:'Napoli',
+        punti_fatti:0,
+        falli_subiti:0,
+    },
+    {
+        nome:'Lecce',
+        punti_fatti:0,
+        falli_subiti:0,
+    },
+ ];
+
+ 
+ const nuovoArraySquadre =[];
+
+ squadre.forEach(squadra => {
+
+    const numeroCasuale1 = Math.floor(Math.random() * 30 + 1);
+    const numeroCasuale2 = Math.floor(Math.random() * 10 + 1);
+
+    squadra.punti_fatti += numeroCasuale1;
+    squadra.falli_subiti += numeroCasuale2;
+
+    nuovoArraySquadre.push(`${squadra.nome} ${squadra.punti_fatti} ${squadra.falli_subiti}`)
+ });
+ 
+ console.log(nuovoArraySquadre);
